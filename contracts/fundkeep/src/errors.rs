e058dddec6e923e0d46256beed63bf479776a37b
mshodipo <michealshodipo56@gmail.com>
2026-09-11 12:04:05 +0100

    feat(contract): add contract error codes

---
+++ contracts/fundkeep/src/errors.rs
@@ -0,0 +1,13 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    GoalNotFound = 1,
+    NotUnlocked = 2,
+    AlreadyWithdrawn = 3,
+    Unauthorized = 4,
+    InvalidAmount = 5,
+    InvalidDeadline = 6,
+}
